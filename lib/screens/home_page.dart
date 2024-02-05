efe2e15b64915650096a3fd10ba461feaebc82c6
jgcoelhof <joao.gabriel.coelho03@aluno.ifce.edu.br>
2024-02-05 00:12:54 -0300

    primeiro commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:poc/controllers/bluetooth_controller.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "POC Flutter Blue Plus",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: GetBuilder<BluetoothController>(
+        init: BluetoothController(),
+        builder: (controller) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 25,
+                ),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.purpleAccent,
+                        backgroundColor: Colors.purple,
+                        minimumSize: const Size(350, 55),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5)))),
+                    onPressed: ()=>controller.scanDevices(),
+                    child: const Text(
+                      'Procurar',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                StreamBuilder<List<ScanResult>>(
+                    stream: controller.scanResults,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            final data = snapshot.data![index];
+                            return Card(
+                              elevation: 2,
+                              child: ListTile(
+                                title: Text(data.device.advName),
+                                subtitle: Text(data.device.id.id),
+                                trailing: Text(
+                                  data.rssi.toString(),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        return const Center(
+                            child: Text("Nenhum dispositivo encontrado!"));
+                      }
+                    })
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
